195008b6bb4e1b53f472aaa034f462e7aa677d01
MaluseLek <maluselekalakala@gmail.com>
2025-09-13 17:10:41 +0200

    Add python_intro directory with simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,6 @@
+principal = 1000    # $1000 principal amount
+rate = 0.05            # 5% annual interest rate 
+time = 3            # 3 years
+
+interest = principal * rate * time
+print("The simple interest is", interest)
