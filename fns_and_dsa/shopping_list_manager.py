6c6e50e5e5d44051332da7a779642df5d22adfe2
MaluseLek <maluselekalakala@gmail.com>
2025-10-20 13:54:57 +0200

    Add Shopping List Manager file corrections.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -6,6 +6,9 @@ shopping_list = []
 def add_item(list_item, shopping_list):
     if list_item not in shopping_list:
         shopping_list.append(list_item)
+        print(f"{list_item} has been added to the shopping list.")
+    else:
+        raise ValueError(f"{list_item} is already in the shopping list.")
     
 
 def remove_item(list_item, shopping_list):
