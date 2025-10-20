1c4b036eec1c3afbf814222687b6395f467fd8fe
MaluseLek <maluselekalakala@gmail.com>
2025-10-20 14:27:01 +0200

    Added Shopping List Manager file corrections.

--- fns_and_dsa/shopping_list_manager.py
+++ fns_and_dsa/shopping_list_manager.py
@@ -23,4 +23,46 @@ def display_list():
     for item in shopping_list:
         print(f"{count}. {item}")
         count += 1
+
+
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            # ADD
+            item_name = input("Enter the name of item to add: ")
+            add_item(item_name, shopping_list)
+            print(f"{item_name} has been added to the Shopping List.")
+            pass
+        elif choice == '2':
+            # Remove
+            item_name = input("Enter the name of item to remove from Shopping List: ")
+            try:
+                remove_item(item_name, shopping_list)
+            except ItemNotFoundError as e:
+                print(e)
+
+            pass
+        elif choice == '3':
+            # Display
+            display_list(shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
     
