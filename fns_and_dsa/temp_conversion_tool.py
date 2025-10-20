e42240403cf714e1f462c55fe722328da92457ff
MaluseLek <maluselekalakala@gmail.com>
2025-10-20 14:21:56 +0200

    Added Temperature Conversion Tool file corrections.

--- fns_and_dsa/temp_conversion_tool.py
+++ fns_and_dsa/temp_conversion_tool.py
@@ -9,14 +9,18 @@ def convert_to_celsius(fahrenheit):
 def convert_to_fahrenheit(celsius):
     return (celsius * CELSIUS_TO_FAHRENHEIT_FACTOR) + 32
 
-temperature = float(input("Enter the temperature to convert: "))
-scale = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
 
-if scale.upper() == "C":
-    converted = convert_to_fahrenheit(temperature)
-    print(f"{temperature}°C is equal to {converted:.2f}°F")
-elif scale.upper() == "F":
-    converted = convert_to_celsius(temperature)
-    print(f"{temperature}°F is equal to {converted:.2f}°C")
-else:
-    print("Invalid temperature scale. Please enter 'C' or 'F'.")
+try:
+    temperature = float(input("Enter the temperature to convert: "))
+    scale = input("Is this temperature in Celsius or Fahrenheit? (C/F): ")
+
+    if scale.upper() == "C":
+        converted = convert_to_fahrenheit(temperature)
+        print(f"{temperature}°C is equal to {converted:.2f}°F")
+    elif scale.upper() == "F":
+        converted = convert_to_celsius(temperature)
+        print(f"{temperature}°F is equal to {converted:.2f}°C")
+    else:
+        print("Invalid temperature scale. Please enter 'C' or 'F'.")
+except ValueError:
+    print("Invalid temperature. Please enter a numeric value.")
