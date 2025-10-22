f9151a4f0b1a4a137bbb79aa91e68e182fe20433
MaluseLek <maluselekalakala@gmail.com>
2025-10-22 23:52:29 +0200

    Add Simple Calculator Test file to repo.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -12,20 +12,17 @@ class TestSimpleCalculator(unittest.TestCase):
         self.assertEqual(self.calc.add(-1, 1), 0)
         self.assertEqual(self.calc.add(0, 0), 0)
 
-    def test_subtract(self):
-        """Test the subtract method."""
+    def test_subtraction(self):
         self.assertEqual(self.calc.subtract(5, 3), 2)
         self.assertEqual(self.calc.subtract(10, 5), 5)
         self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
-        """Test the multiply method."""
         self.assertEqual(self.calc.multiply(2, 3), 6)
         self.assertEqual(self.calc.multiply(-1, 5), -5)
         self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
-        """Test the divide method."""
         self.assertEqual(self.calc.divide(6, 2), 3)
         self.assertEqual(self.calc.divide(10, 2), 5)
         self.assertEqual(self.calc.divide(255, 5), 51)
