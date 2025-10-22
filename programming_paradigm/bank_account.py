23a635f95db228d06956fc8aa6cbc8d6513d166a
MaluseLek <maluselekalakala@gmail.com>
2025-10-22 19:38:55 +0200

    Bank Account class added to repo.

--- programming_paradigm/bank_account.py
+++ programming_paradigm/bank_account.py
@@ -14,31 +14,7 @@ class BankAccount:
             return False 
 
     def display_balance(self):
-        print(f"Balance: R{self.account_balance}")
+        print(f"Current Balance: R{self.account_balance}")
 
 
-    """
-    def __init__(self, account_number: str, account_holder: str, balance: float = 0.0):
-        self.account_number = account_number
-        self.account_holder = account_holder
-    #     self.balance = balance
-
-    # def deposit(self, amount: float) -> None:
-    #     if amount <= 0:
-    #         raise ValueError("Deposit amount must be positive.")
-    #     self.balance += amount
-
-    # def withdraw(self, amount: float) -> None:
-    #     if amount <= 0:
-    #         raise ValueError("Withdrawal amount must be positive.")
-    #     if amount > self.balance:
-    #         raise ValueError("Insufficient funds.")
-    #     self.balance -= amount
-
-    # def get_balance(self) -> float:
-    #     return self.balance
-
-    # def __str__(self) -> str:
-    #     return f"Account {self.account_number} held by {self.account_holder} has a balance of ${self.balance:.2f}"
-
-""" 
+    
