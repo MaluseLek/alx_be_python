dccbedb3e9e184811b679209e4f69a08b3240147
MaluseLek <maluselekalakala@gmail.com>
2025-10-22 23:50:39 +0200

    Add Simple Calculator Test file to repo.

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -5,32 +5,31 @@ class TestSimpleCalculator(unittest.TestCase):
 
     def setUp(self):
         """Set up the SimpleCalculator instance before each test."""
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_addition(self):
-        """Test the add method."""
-        self.assertEqual(self.calculator.add(2, 3), 5)
-        self.assertEqual(self.calculator.add(-1, 1), 0)
-        self.assertEqual(self.calculator.add(0, 0), 0)
+    def test_add(self):
+        self.assertEqual(self.calc.add(2, 3), 5)
+        self.assertEqual(self.calc.add(-1, 1), 0)
+        self.assertEqual(self.calc.add(0, 0), 0)
 
     def test_subtract(self):
         """Test the subtract method."""
-        self.assertEqual(self.calculator.subtract(5, 3), 2)
-        self.assertEqual(self.calculator.subtract(10, 5), 5)
-        self.assertEqual(self.calculator.subtract(0, 0), 0)
+        self.assertEqual(self.calc.subtract(5, 3), 2)
+        self.assertEqual(self.calc.subtract(10, 5), 5)
+        self.assertEqual(self.calc.subtract(0, 0), 0)
 
     def test_multiply(self):
         """Test the multiply method."""
-        self.assertEqual(self.calculator.multiply(2, 3), 6)
-        self.assertEqual(self.calculator.multiply(-1, 5), -5)
-        self.assertEqual(self.calculator.multiply(0, 10), 0)
+        self.assertEqual(self.calc.multiply(2, 3), 6)
+        self.assertEqual(self.calc.multiply(-1, 5), -5)
+        self.assertEqual(self.calc.multiply(0, 10), 0)
 
     def test_divide(self):
         """Test the divide method."""
-        self.assertEqual(self.calculator.divide(6, 2), 3)
-        self.assertEqual(self.calculator.divide(10, 2), 5)
-        self.assertEqual(self.calculator.divide(0, 5), 0)
-        
+        self.assertEqual(self.calc.divide(6, 2), 3)
+        self.assertEqual(self.calc.divide(10, 2), 5)
+        self.assertEqual(self.calc.divide(255, 5), 51)
+        self.assertIsNone(self.calc.divide(10, 0), "Division by zero should return None")
 
 if __name__ == '__main__':
     unittest.main()
